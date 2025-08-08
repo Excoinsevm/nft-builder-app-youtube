26943acb614f25b0f248f5dc931c806e3051cf5a
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2025-08-08 05:07:32 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,18 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import type { Chain } from "@thirdweb-dev/chains";
+import { bitrock } from "./chains";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const bitrockChain: Chain = {
-  chainId: 7171,
-  chain: "bitrock",
-  name: "Bitrock Mainnet",
-  shortName: "brock",
-  slug: "bitrock",
-  nativeCurrency: {
-    name: "Bitrock",
-    symbol: "BROCK",
-    decimals: 18,
-  },
-  rpc: ["https://connect.bit-rock.io"], // RPC endpoint
-  explorers: {
-    default: {
-      name: "Bitrock Explorer",
-      url: "https://explorer.bit-rock.io",
-    },
-  },
-  testnet: false,
-};
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={bitrockChain}
+      activeChain={bitrock}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
